5c3fee1d7c3de273d601759461da78d3491dfe3d
Dominik Živko <domy.zivko@gmail.com>
2022-02-18 21:23:36 +0100

    Add 'Search returns result' test

--- src/test/kotlin/hr/ferit/dominikzivko/hgtest/GenericTests.kt
+++ src/test/kotlin/hr/ferit/dominikzivko/hgtest/GenericTests.kt
@@ -1,7 +1,16 @@
 package hr.ferit.dominikzivko.hgtest
 
 import hr.ferit.dominikzivko.hgtest.model.HomePage
+import org.testng.annotations.Test
 
 class GenericTests : HgShopTests(HomePage.URL) {
 
+    @Test
+    fun `Search returns result`() {
+        val searchQuery = "kabel apple usb-c"
+        val homePage = HomePage(driver)
+        val resultsPage = homePage.searchArticles(searchQuery)
+        val firstResult = resultsPage.searchResults.first()
+        assert(firstResult.name.startsWith("kabel apple usb-c", ignoreCase = true))
+    }
 }
